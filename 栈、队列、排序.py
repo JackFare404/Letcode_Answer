fe90dcc72a10aeecc1c7e020819d5d645ff852f8
bigot_404 <37007395+JackFare404@users.noreply.github.com>
2023-01-06 17:14:20 +0800

    Create 栈、队列、排序.py

---
+++ 栈、队列、排序.py
@@ -0,0 +1,160 @@
+""""""
+'''
+20. 有效的括号
+给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+有效字符串需满足：
+左括号必须用相同类型的右括号闭合。
+左括号必须以正确的顺序闭合。
+每个右括号都有一个对应的相同类型的左括号。
+
+示例 1：
+输入：s = "()"
+输出：true
+示例 2：
+输入：s = "()[]{}"
+输出：true
+示例 3：
+输入：s = "(]"
+输出：false
+'''
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        dic = {')': '(', ']': '[', '}': '{'}
+        res = []
+        for i in s:
+            if res and i in dic:  #判断res是否存在，边界问题，只有一个字符串
+                if dic[i] == res[-1]:
+                    res.pop()
+                else:return False
+            else:res.append(i)
+        return not res
+
+
+'''
+155. 最小栈
+设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+实现 MinStack 类:
+MinStack() 初始化堆栈对象。
+void push(int val) 将元素val推入堆栈。
+void pop() 删除堆栈顶部的元素。
+int top() 获取堆栈顶部的元素。
+int getMin() 获取堆栈中的最小元素。
+
+示例 1:
+输入：
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+输出：
+[null,null,null,null,-3,null,0,-2]
+解释：
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin();   --> 返回 -3.
+minStack.pop();
+minStack.top();      --> 返回 0.
+minStack.getMin();   --> 返回 -2.
+'''
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minstack = [inf]
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        self.minstack.append(min(self.minstack[-1], val))
+
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minstack.pop()
+
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minstack[-1]
+
+'''
+933. 最近的请求次数
+写一个 RecentCounter 类来计算特定时间范围内最近的请求。
+请你实现 RecentCounter 类：
+RecentCounter() 初始化计数器，请求数为 0 。
+int ping(int t) 在时间 t 添加一个新请求，其中 t 表示以毫秒为单位的某个时间，并返回过去 3000 毫秒内发生的所有请求数（包括新请求）。确切地说，返回在 [t-3000, t] 内发生的请求数。
+保证 每次对 ping 的调用都使用比之前更大的 t 值。
+
+示例 1：
+输入：
+["RecentCounter", "ping", "ping", "ping", "ping"]
+[[], [1], [100], [3001], [3002]]
+输出：
+[null, 1, 2, 3, 3]
+
+解释：
+RecentCounter recentCounter = new RecentCounter();
+recentCounter.ping(1);     // requests = [1]，范围是 [-2999,1]，返回 1
+recentCounter.ping(100);   // requests = [1, 100]，范围是 [-2900,100]，返回 2
+recentCounter.ping(3001);  // requests = [1, 100, 3001]，范围是 [1,3001]，返回 3
+recentCounter.ping(3002);  // requests = [1, 100, 3001, 3002]，范围是 [2,3002]，返回 3
+'''
+class RecentCounter:
+
+    def __init__(self):
+        self.q = []
+
+    def ping(self, t: int) -> int:
+        self.q.append(t)
+        while self.q[0] < t - 3000:
+            self.q.pop(0)
+        return len(self.q)
+
+
+'''
+1370. 上升下降字符串
+给你一个字符串 s ，请你根据下面的算法重新构造字符串：
+从 s 中选出 最小 的字符，将它 接在 结果字符串的后面。
+从 s 剩余字符中选出 最小 的字符，且该字符比上一个添加的字符大，将它 接在 结果字符串后面。
+重复步骤 2 ，直到你没法从 s 中选择字符。
+从 s 中选出 最大 的字符，将它 接在 结果字符串的后面。
+从 s 剩余字符中选出 最大 的字符，且该字符比上一个添加的字符小，将它 接在 结果字符串后面。
+重复步骤 5 ，直到你没法从 s 中选择字符。
+重复步骤 1 到 6 ，直到 s 中所有字符都已经被选过。
+在任何一步中，如果最小或者最大字符不止一个 ，你可以选择其中任意一个，并将其添加到结果字符串。
+请你返回将 s 中字符重新排序后的 结果字符串 。
+
+示例 1：
+输入：s = "aaaabbbbcccc"
+输出："abccbaabccba"
+解释：第一轮的步骤 1，2，3 后，结果字符串为 result = "abc"
+第一轮的步骤 4，5，6 后，结果字符串为 result = "abccba"
+第一轮结束，现在 s = "aabbcc" ，我们再次回到步骤 1
+第二轮的步骤 1，2，3 后，结果字符串为 result = "abccbaabc"
+第二轮的步骤 4，5，6 后，结果字符串为 result = "abccbaabccba"
+示例 2：
+输入：s = "rat"
+输出："art"
+解释：单词 "rat" 在上述算法重排序以后变成 "art"
+示例 3：
+输入：s = "leetcode"
+输出："cdelotee"
+示例 4：
+输入：s = "ggggggg"
+输出："ggggggg"
+示例 5：
+输入：s = "spo"
+输出："ops"
+'''
+
+'''
+
+'''
+
+'''
+
+'''
