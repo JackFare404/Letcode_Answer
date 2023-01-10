c609fb30d68242f2918fdf47d41d999f7c51e7f1
bigot_404 <37007395+JackFare404@users.noreply.github.com>
2023-01-10 15:41:41 +0800

    Update 贪心算法.py

--- 贪心算法.py
+++ 贪心算法.py
@@ -1,1 +1,168 @@
+""""""
+'''
+121. 买卖股票的最佳时机
+给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
 
+示例 1：
+输入：[7,1,5,3,6,4]
+输出：5
+解释：在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+     注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格；同时，你不能在买入前卖出股票。
+示例 2：
+输入：prices = [7,6,4,3,1]
+输出：0
+解释：在这种情况下, 没有交易完成, 所以最大利润为 0。
+'''
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # inf = int(1e9)
+        minp = math.inf
+        maxchazhi = 0
+        for price in prices:
+            minp = min(price, minp)
+            maxchazhi = max(price - minp, maxchazhi)
+        return maxchazhi
+
+
+'''
+435. 无重叠区间
+给定一个区间的集合 intervals ，其中 intervals[i] = [starti, endi] 。返回 需要移除区间的最小数量，使剩余区间互不重叠 。
+
+示例 1:
+输入: intervals = [[1,2],[2,3],[3,4],[1,3]]
+输出: 1
+解释: 移除 [1,3] 后，剩下的区间没有重叠。
+示例 2:
+输入: intervals = [ [1,2], [1,2], [1,2] ]
+输出: 2
+解释: 你需要移除两个 [1,2] 来使剩下的区间没有重叠。
+示例 3:
+输入: intervals = [ [1,2], [2,3] ]
+输出: 0
+解释: 你不需要移除任何区间，因为它们已经是无重叠的了。
+'''
+
+
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda x: x[1])
+        n = len(intervals)
+        right = intervals[0][1]
+        res = 1
+        for i in range(1, n):
+            if intervals[i][0] >= right:
+                res += 1
+                right = intervals[i][1]
+        return n - res
+
+
+'''
+452. 用最少数量的箭引爆气球
+有一些球形气球贴在一堵用 XY 平面表示的墙面上。墙面上的气球记录在整数数组 points ，其中points[i] = [xstart, xend] 表示水平直径在 xstart 和 xend之间的气球。你不知道气球的确切 y 坐标。
+一支弓箭可以沿着 x 轴从不同点 完全垂直 地射出。在坐标 x 处射出一支箭，若有一个气球的直径的开始和结束坐标为 xstart，xend， 且满足  xstart ≤ x ≤ xend，则该气球会被 引爆 。可以射出的弓箭的数量 没有限制 。 弓箭一旦被射出之后，可以无限地前进。
+给你一个数组 points ，返回引爆所有气球所必须射出的 最小 弓箭数 。
+ 
+示例 1：
+输入：points = [[10,16],[2,8],[1,6],[7,12]]
+输出：2
+解释：气球可以用2支箭来爆破:
+-在x = 6处射出箭，击破气球[2,8]和[1,6]。
+-在x = 11处发射箭，击破气球[10,16]和[7,12]。
+示例 2：
+输入：points = [[1,2],[3,4],[5,6],[7,8]]
+输出：4
+解释：每个气球需要射出一支箭，总共需要4支箭。
+示例 3：
+输入：points = [[1,2],[2,3],[3,4],[4,5]]
+输出：2
+解释：气球可以用2支箭来爆破:
+- 在x = 2处发射箭，击破气球[1,2]和[2,3]。
+- 在x = 4处射出箭，击破气球[3,4]和[4,5]。
+'''
+
+
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        points.sort(key = lambda x : x[1])
+        rightpoint = points[0][1]
+        ans = 1
+        for point in points:
+            if point[0] > rightpoint:
+                rightpoint = point[1]
+                ans += 1
+        return ans
+
+
+
+'''
+455. 分发饼干
+假设你是一位很棒的家长，想要给你的孩子们一些小饼干。但是，每个孩子最多只能给一块饼干。
+对每个孩子 i，都有一个胃口值 g[i]，这是能让孩子们满足胃口的饼干的最小尺寸；并且每块饼干 j，都有一个尺寸 s[j] 。如果 s[j] >= g[i]，我们可以将这个饼干 j 分配给孩子 i ，这个孩子会得到满足。你的目标是尽可能满足越多数量的孩子，并输出这个最大数值。
+ 
+示例 1:
+输入: g = [1,2,3], s = [1,1]
+输出: 1
+解释: 
+你有三个孩子和两块小饼干，3个孩子的胃口值分别是：1,2,3。
+虽然你有两块小饼干，由于他们的尺寸都是1，你只能让胃口值是1的孩子满足。
+所以你应该输出1。
+示例 2:
+输入: g = [1,2], s = [1,2,3]
+输出: 2
+解释: 
+你有两个孩子和三块小饼干，2个孩子的胃口值分别是1,2。
+你拥有的饼干数量和尺寸都足以让所有孩子满足。
+所以你应该输出2.
+'''
+
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        m, n = len(g), len(s)
+        i = j = count = 0
+        while i < m and j < n:
+            if g[i] <= s[j]:
+                count += 1
+                i += 1
+                j += 1
+            else:
+                j += 1
+        return count
+
+'''
+763. 划分字母区间
+给你一个字符串 s 。我们要把这个字符串划分为尽可能多的片段，同一字母最多出现在一个片段中。
+注意，划分结果需要满足：将所有划分结果按顺序连接，得到的字符串仍然是 s 。
+返回一个表示每个字符串片段的长度的列表。
+
+示例 1：
+输入：s = "ababcbacadefegdehijhklij"
+输出：[9,7,8]
+解释：
+划分结果为 "ababcbaca"、"defegde"、"hijhklij" 。
+每个字母最多出现在一个片段中。
+像 "ababcbacadefegde", "hijhklij" 这样的划分是错误的，因为划分的片段数较少。 
+示例 2：
+输入：s = "eccbbbbdec"
+输出：[10]
+'''
+
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        lastpos = [0] * 26
+        for i, j in enumerate(s):
+            lastpos[ord(j) - ord('a')] = i
+
+        ans = []
+        start = end = 0
+        for i, j in enumerate(s):
+            end = max(end, lastpos[ord(j) - ord('a')])
+            if i == end:
+                ans.append(end - start + 1)
+                start = end + 1
+        return ans
