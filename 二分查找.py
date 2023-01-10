72996c694d3b4e0c5623b0939df28ed8358fed1d
bigot_404 <37007395+JackFare404@users.noreply.github.com>
2023-01-10 20:22:51 +0800

    Create 二分查找.py

---
+++ 二分查找.py
@@ -0,0 +1,162 @@
+""""""
+'''
+34. 在排序数组中查找元素的第一个和最后一个位置
+给你一个按照非递减顺序排列的整数数组 nums，和一个目标值 target。请你找出给定目标值在数组中的开始位置和结束位置。
+如果数组中不存在目标值 target，返回 [-1, -1]。
+你必须设计并实现时间复杂度为 O(log n) 的算法解决此问题。
+
+示例 1：
+输入：nums = [5,7,7,8,8,10], target = 8
+输出：[3,4]
+示例 2：
+输入：nums = [5,7,7,8,8,10], target = 6
+输出：[-1,-1]
+示例 3：
+输入：nums = [], target = 0
+输出：[-1,-1]
+'''
+
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        def leftzone(nums, target):
+            left, right = 0, len(nums) - 1
+            while left <= right:
+                mid = (left + right) // 2
+                if nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return left
+
+        left = leftzone(nums, target)
+        right = leftzone(nums, target + 1) - 1
+        if left >= len(nums) or nums[left] != target:
+            return [-1, -1]
+        else:
+            return [left, right]
+
+'''
+69. x 的平方根 
+给你一个非负整数 x ，计算并返回 x 的 算术平方根 。
+由于返回类型是整数，结果只保留 整数部分 ，小数部分将被 舍去 。
+注意：不允许使用任何内置指数函数和算符，例如 pow(x, 0.5) 或者 x ** 0.5 。
+ 
+示例 1：
+输入：x = 4
+输出：2
+示例 2：
+输入：x = 8
+输出：2
+解释：8 的算术平方根是 2.82842..., 由于返回类型是整数，小数部分将被舍去。
+'''
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        left, right = 0, x
+        ans = 0
+        while left <= right:
+            mid = (left + right) // 2
+            if mid**2 <= x:
+                ans = mid
+                left = mid + 1
+            else :
+                right = mid - 1
+        return ans
+
+'''
+153. 寻找旋转排序数组中的最小值
+已知一个长度为 n 的数组，预先按照升序排列，经由 1 到 n 次 旋转 后，得到输入数组。例如，原数组 nums = [0,1,2,4,5,6,7] 在变化后可能得到：
+若旋转 4 次，则可以得到 [4,5,6,7,0,1,2]
+若旋转 7 次，则可以得到 [0,1,2,4,5,6,7]
+注意，数组 [a[0], a[1], a[2], ..., a[n-1]] 旋转一次 的结果为数组 [a[n-1], a[0], a[1], a[2], ..., a[n-2]] 。
+给你一个元素值 互不相同 的数组 nums ，它原来是一个升序排列的数组，并按上述情形进行了多次旋转。请你找出并返回数组中的 最小元素 。
+你必须设计一个时间复杂度为 O(log n) 的算法解决此问题。
+
+示例 1：
+输入：nums = [3,4,5,1,2]
+输出：1
+解释：原数组为 [1,2,3,4,5] ，旋转 3 次得到输入数组。
+示例 2：
+输入：nums = [4,5,6,7,0,1,2]
+输出：0
+解释：原数组为 [0,1,2,4,5,6,7] ，旋转 4 次得到输入数组。
+示例 3：
+输入：nums = [11,13,15,17]
+输出：11
+解释：原数组为 [11,13,15,17] ，旋转 4 次得到输入数组。
+'''
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        low, high = 0, len(nums) - 1
+        while low < high:  #由于数组不包含重复元素，并且只要当前的区间长度不为 11，\it pivotpivot 就不会与 \it highhigh 重合；而如果当前的区间长度为 11，这说明我们已经可以结束二分查找了。因此不会存在 \textit{nums}[\textit{pivot}] = \textit{nums}[\textit{high}]nums[pivot]=nums[high] 的情况。
+            mid = (low + high) // 2
+            if nums[mid] < nums[high]:
+                high = mid
+            else:
+                low = mid + 1
+        return nums[low]
+
+'''
+278. 第一个错误的版本
+你是产品经理，目前正在带领一个团队开发新的产品。不幸的是，你的产品的最新版本没有通过质量检测。由于每个版本都是基于之前的版本开发的，所以错误的版本之后的所有版本都是错的。
+假设你有 n 个版本 [1, 2, ..., n]，你想找出导致之后所有版本出错的第一个错误的版本。
+你可以通过调用 bool isBadVersion(version) 接口来判断版本号 version 是否在单元测试中出错。实现一个函数来查找第一个错误的版本。你应该尽量减少对调用 API 的次数。
+ 
+示例 1：
+输入：n = 5, bad = 4
+输出：4
+解释：
+调用 isBadVersion(3) -> false 
+调用 isBadVersion(5) -> true 
+调用 isBadVersion(4) -> true
+所以，4 是第一个错误的版本。
+示例 2：
+输入：n = 1, bad = 1
+输出：1
+'''
+
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        left = 1
+        right = n
+        while left < right:
+            mid = (left + right) // 2
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left = mid + 1
+        return left
+
+'''
+540. 有序数组中的单一元素
+给你一个仅由整数组成的有序数组，其中每个元素都会出现两次，唯有一个数只会出现一次。
+请你找出并返回只出现一次的那个数。
+你设计的解决方案必须满足 O(log n) 时间复杂度和 O(1) 空间复杂度。
+
+示例 1:
+输入: nums = [1,1,2,3,3,4,4,8,8]
+输出: 2
+示例 2:
+输入: nums =  [3,3,7,7,10,11,11]
+输出: 10
+'''
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        left, right = 0, len(nums) - 1
+        while left < right:
+            mid = (left + right) //2
+            if nums[mid] == nums[mid ^ 1]:
+                left = mid + 1
+            else:
+                right = mid
+        return nums[left]
+
+
+        # 根据题目描述，数组应该呈现为nums[0] == nums[1], nums[2] == nums[3]的形式，也就是nums[i] == nums[i^1]
+
+ # [3,3,7,7,10,11,11]
+ #  0 1 2 3 4  5  6
